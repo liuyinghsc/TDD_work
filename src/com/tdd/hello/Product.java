869eb50496f4a1e4d2c76bc309101781a587a4cf
ying.liu <1292474120@qq.com>
2020-06-08 21:48:11 +0800

    all_products

---
+++ src/com/tdd/hello/Product.java
@@ -0,0 +1,35 @@
+package com.tdd.hello;
+
+public class Product {
+
+    private int sellin;
+    private int quality;
+    private boolean flag;
+
+    public Product() {
+    }
+
+    public int getSellin() {
+        return sellin;
+    }
+
+    public void setSellin(int sellin) {
+        this.sellin = sellin;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+}
--- src/com/tdd/test/TDDTest.java
+++ src/com/tdd/test/TDDTest.java
@@ -1,7 +1,51 @@
 package com.tdd.test;
 
+import com.tdd.hello.Product;
+
+import java.util.Optional;
+
 public class TDDTest {
+
+
+    public int getValue(Product product){
+//        普通商品
+        if (!product.isFlag()){
+            if (product.getSellin()>=0){
+                if (product.getQuality()>=1&&product.getQuality()<=50){
+                    product.setQuality(product.getQuality()-1);
+                }
+            }else {
+                if (product.getQuality()>=1&&product.getQuality()<=50){
+                    product.setQuality(product.getQuality()-2);
+                }
+            }
+        }else {
+//            特殊商品
+            if (product.getSellin()>10){
+                if (product.getQuality()>=1&&product.getQuality()<=50){
+                    product.setQuality(product.getQuality()-1);
+                }
+            }else if(product.getSellin()>5&&product.getSellin()<=10){
+                if (product.getQuality()>=1&&product.getQuality()<=50){
+                    product.setQuality(product.getQuality()+2);
+                }
+            }
+            else if (product.getSellin()>=0&&product.getSellin()<=5){
+                if (product.getQuality()>=1&&product.getQuality()<=50){
+                    product.setQuality(product.getQuality()+3);
+                }
+            }else {
+                product.setQuality(0);
+
+            }
+
+        }
+        return product.getQuality();
+
+    }
+
 
 
 
 }
+
